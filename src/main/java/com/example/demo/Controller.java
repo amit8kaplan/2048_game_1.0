7b512c652582e3524e1b4e1d1365cacf1e2b1fe1
Amit Kaplan <amit.y.kaplan@gmail.com>
2023-12-25 21:26:15 +0200

    first work with sceneBuilder

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,14 +1,19 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
-
+    Canvas board_Game;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void start(){
+        System.out.println("start");
     }
 }
