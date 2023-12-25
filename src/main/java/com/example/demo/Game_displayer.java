cc8c34b55e045c27f1c2a313d7477bafed66f02e
Amit Kaplan <amit.y.kaplan@gmail.com>
2023-12-25 21:57:51 +0200

    func in Game_displayer the func is to set again the board game when its chage. for first attempt - i only print it, and dont change

--- src/main/java/com/example/demo/Game_displayer.java
+++ src/main/java/com/example/demo/Game_displayer.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
 import java.util.Random;
 
 public class Game_displayer extends Canvas {
-    int [][] game_data;
-    private int singelton =0 ;
+    int [][] game_data = new int[3][3];
+    private int singleton =0 ;
     public Game_displayer(int x){
+
         int[] possibleValues = {2, 4, 8, 16};
         Random rand = new Random();
         int rand_row, rand_col, rand_val;
@@ -17,6 +19,7 @@ public class Game_displayer extends Canvas {
             rand_val = rand.nextInt(possibleValues.length);
             game_data[rand_row][rand_col] = rand_val;
         }
+
     }
     public Game_displayer(int[][] game_data) {
         this.game_data = game_data;
@@ -31,15 +34,28 @@ public class Game_displayer extends Canvas {
         this.game_data = game_data;
     }
     public Game_displayer start(int x){
-        if (singelton !=0)
+        if (singleton !=0)
+        {
+            singleton++;
             return this;
-        else{
-
         }
+            return new Game_displayer(x);
     }
 
-    public void change_touch(){
+    public void change_touch_redarw(int x){
+//        for start it always gets up
+        if (singleton!=0){
+            double W = getWidth();
+            double H = getHeight();
+            double in = W/3;
 
+            GraphicsContext gc = getGraphicsContext2D();
+            for (int row = 0 ; row<3; row++)
+                for (int col = 0; col <3; col++){
+                    if (game_data[row][col]!=0)
+                        gc.fillText(String.valueOf(game_data[row][col]), in, in);
+                }
+        }
     }
 
 
